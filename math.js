bb0f8b8fdd91fe773787ecdc0172f5cb4fa4c557
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-07 17:04:10 +0000

    Created a module and a separate js file to require it

---
+++ math.js
@@ -0,0 +1,25 @@
+
+// An example of a module exporting functions.
+
+// Flips a number's sign to Negative
+function flipToNegative(n) {
+  if (Math.sign(n) == 1) {
+    return n * -1;
+  }
+  else {
+    return n;
+  }
+}
+
+module.exports {
+  // Returns x given a, b, and c according to the Quadratic formula.
+  quadratic: function (a, b, c) {
+    var result = {};
+
+    result.x1 = (flipToNegative(b) + (Math.sqrt((Math.pow(b, 2)) - 4 * a * c))) / (2*a);
+    result.x2 = (flipToNegative(b) - (Math.sqrt((Math.pow(b, 2)) - 4 * a * c))) / (2*a);
+
+    return result;
+
+  }
+}
---
+++ moduleExample.js
@@ -0,0 +1,7 @@
+
+var algebra = require("./math");
+
+// Call the quadratic formula.
+algebra.quadratic(Math.floor(Math.random()*8)+1, Math.floor(Math.random()*4)+1);
+
+console.log(algebra);
